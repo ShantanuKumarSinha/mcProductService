5c81504b9086054d066e147423bddfaeae4aa73b
Shantanu Kumar <shan.raj93@gmail.com>
2024-11-22 12:13:50 +0530

    (refactor/mailSetup): add mailHog host and port for local smtp setup

--- src/test/java/dev/shann/mcproductservice/ProductServiceTest.java
+++ src/test/java/dev/shann/mcproductservice/ProductServiceTest.java
@@ -21,7 +21,6 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.tuple;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.*;
