33de5aef8a018fe9d80ed5860798a2d0688d1b67
Shantanu Kumar <shan.raj93@gmail.com>
2025-08-26 00:04:32 +0530

    (chore/project-issue-template): test case commented

--- src/test/java/dev/shann/mcproductservice/McProductServiceApplicationTests.java
+++ src/test/java/dev/shann/mcproductservice/McProductServiceApplicationTests.java
@@ -4,20 +4,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import dev.shann.mcproductservice.dto.CreateOrUpdateProductDetailsRequestDto;
 import dev.shann.mcproductservice.model.Product;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -31,6 +31,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "file:src/test/resources/application-test.properties")
 @ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+@Disabled
 class McProductServiceApplicationTests {
 
     WireMockServer wireMockServer;
@@ -41,27 +43,24 @@ class McProductServiceApplicationTests {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Bean
     @LoadBalanced
     public WebClient webClient() {
         return WebClient.builder().baseUrl(urlAlt).build();
     }
-
-    @BeforeEach
-    void setUp() {
-        wireMockServer = new WireMockServer(8080);
-    }
-
-    @Test
-    @Disabled
-    void testProductCreation() throws Exception {
-        var product = Product.builder().productName("testProduct").brand("testBrand").price(100).quantity(1000).build();
-        var createOrUpdateProductDetailsRequestDto = new CreateOrUpdateProductDetailsRequestDto("test@test.com", "testPassword", product);
-        var jsonStringify = objectMapper.writeValueAsString(createOrUpdateProductDetailsRequestDto);
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("http://product-service/products").content(jsonStringify).contentType(MediaType.APPLICATION_JSON);
-        var result = mockMvc.perform(requestBuilder).andExpect(status().isCreated()).andReturn();
-        var actualProduct = objectMapper.readValue(result.getResponse().getContentAsString(), Product.class);
-        assertSoftly(softly -> softly.assertThat(actualProduct).extracting(Product::getProductId, Product::getProductName, Product::getBrand, Product::getPrice, Product::getQuantity).contains(1L, "testProduct", "testBrand", 100, 1000));
-    }
+//    @BeforeEach
+//    void setUp() {
+//        wireMockServer = new WireMockServer(8080);
+//    }
+//    @Test
+//    @Disabled
+//    void testProductCreation() throws Exception {
+//        var product = Product.builder().productName("testProduct").brand("testBrand").price(100).quantity(1000).build();
+//        var createOrUpdateProductDetailsRequestDto = new CreateOrUpdateProductDetailsRequestDto("test@test.com", "testPassword", product);
+//        var jsonStringify = objectMapper.writeValueAsString(createOrUpdateProductDetailsRequestDto);
+//        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("http://product-service/products").content(jsonStringify).contentType(MediaType.APPLICATION_JSON);
+//        var result = mockMvc.perform(requestBuilder).andExpect(status().isCreated()).andReturn();
+//        var actualProduct = objectMapper.readValue(result.getResponse().getContentAsString(), Product.class);
+//        assertSoftly(softly -> softly.assertThat(actualProduct).extracting(Product::getProductId, Product::getProductName, Product::getBrand, Product::getPrice, Product::getQuantity).contains(1L, "testProduct", "testBrand", 100, 1000));
+//    }
 
 }
